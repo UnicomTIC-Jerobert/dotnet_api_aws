f7bc0b19529ff0cc27e576ae55679741b3b05f80
unknown <jerobert11unicomtic@gmail.com>
2025-07-15 10:23:42 +0530

    swagger mode on

--- Program.cs
+++ Program.cs
@@ -18,21 +18,28 @@ builder.Logging.AddDebug();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-else
-{
-    // Enable Swagger in development only
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     app.UseHsts();
+// }
+// else
+// {
+//     // Enable Swagger in development only
+//     app.UseSwagger();
+//     app.UseSwaggerUI(options =>
+//     {
+//         options.SwaggerEndpoint("/swagger/v1/swagger.json", "ICEDT API v1");
+//         options.RoutePrefix = "swagger"; // Access at /swagger
+//     });
+// }
+
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "ICEDT API v1");
         options.RoutePrefix = "swagger"; // Access at /swagger
     });
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
